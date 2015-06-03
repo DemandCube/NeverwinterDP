f28a0b2a4190c3fdb1389305c22f2672812a473a
Tuan Nguyen <tuan08@gmail.com>
2015-06-03 15:27:36 +0700

    TODO for Peter

---
+++ client/swingui/src/main/java/com/neverwinterdp/swing/registry/UIActivitiesView.java
@@ -0,0 +1,7 @@
+package com.neverwinterdp.swing.registry;
+
+//TODO: this should show a split plane, the above half should show the list of the activities
+//The bottom half should show the detail information of an activity (the activity steps) and should reuse UIActivityView
+public class UIActivitiesView {
+
+}
