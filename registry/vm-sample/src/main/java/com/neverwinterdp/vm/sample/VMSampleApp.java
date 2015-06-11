58d0c7c5aad5eab1323d96d0081aa683f177e82c
Tuan Nguyen <tuan08@gmail.com>
2015-06-11 20:34:28 +0700

    create global release project, run vm sample app for 30s only

--- registry/vm-sample/src/main/java/com/neverwinterdp/vm/sample/VMSampleApp.java
+++ registry/vm-sample/src/main/java/com/neverwinterdp/vm/sample/VMSampleApp.java
@@ -6,9 +6,11 @@ public class VMSampleApp extends VMApp {
   @Override
   public void run() throws Exception {
     try {
-      while(true) {
+      int count = 0 ;
+      while(count < 30) {
         System.out.println("Hello VM Sample App!!!");
         Thread.sleep(1000);
+        count++ ;
       }
     } catch(InterruptedException ex) {
     }
