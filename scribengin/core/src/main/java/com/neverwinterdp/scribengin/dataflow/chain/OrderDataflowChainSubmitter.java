9398ad414ae5555fef5ab0f02f12001a26185548
Tuan Nguyen <tuan08@gmail.com>
2015-07-20 21:37:56 +0700

    hack to debug problem

--- scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/chain/OrderDataflowChainSubmitter.java
+++ scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/chain/OrderDataflowChainSubmitter.java
@@ -31,6 +31,9 @@ public class OrderDataflowChainSubmitter extends DataflowChainSubmitter {
     submitter.submit();
     setupDebugger(submitter);
     submitter.waitForRunning(timeout);
+    if(descriptor.getId().indexOf("splitter") > 0) {
+      Thread.sleep(60000);
+    }
     return submitter;
   }
   
