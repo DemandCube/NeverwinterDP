0c956c80d337f9e813dcc56e7b35175f1feca32b
Tuan Nguyen <tuan08@gmail.com>
2015-07-22 01:00:31 +0700

    fix commit size to avoid out of memory for S3

--- scribengin/dataflow/log-sample/src/main/java/com/neverwinterdp/dataflow/logsample/LogMessagePerister.java
+++ scribengin/dataflow/log-sample/src/main/java/com/neverwinterdp/dataflow/logsample/LogMessagePerister.java
@@ -5,10 +5,17 @@ import com.neverwinterdp.scribengin.dataflow.DataflowTaskContext;
 import com.neverwinterdp.scribengin.scribe.ScribeAbstract;
 
 public class LogMessagePerister extends ScribeAbstract {
+  int count = 0 ;
+  
   public void process(DataflowMessage dflMessage, DataflowTaskContext ctx) throws Exception {
     String[] sink = ctx.getAvailableSinks();
     for(String selSink : sink) {
       ctx.write(selSink, dflMessage);
+    }
+    
+    count++ ;
+    if(count > 0 && count % 10000 == 0) {
+      ctx.commit();
     }
   }
 }
