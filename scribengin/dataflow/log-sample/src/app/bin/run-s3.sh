0fc58920afde933717e639a10bc7fc6a1da63578
Tuan Nguyen <tuan08@gmail.com>
2015-07-22 16:18:41 +0700

    change test params, increase submit timeout

--- scribengin/dataflow/log-sample/src/app/bin/run-s3.sh
+++ scribengin/dataflow/log-sample/src/app/bin/run-s3.sh
@@ -59,7 +59,7 @@ if [ "$PROFILE" = "performance" ] ; then
     --log-generator-num-of-message $NUM_OF_MESSAGE --log-generator-message-size $MESSAGE_SIZE \
     --log-validator-wait-for-termination 600000 --log-validator-validate-s3 test-log-sample:info,test-log-sample:warn,test-log-sample:error \
     --dataflow-descriptor $APP_DIR/conf/s3-log-dataflow-chain.json  \
-    --dataflow-wait-for-submit-timeout 150000 --dataflow-wait-for-termination-timeout $MAX_RUN_TIME \
+    --dataflow-wait-for-submit-timeout 210000 --dataflow-wait-for-termination-timeout $MAX_RUN_TIME \
     --dataflow-task-debug
 elif [ "$PROFILE" = "dataflow-worker-failure" ] ; then
   MAX_RUN_TIME=$(( 180000 + ($NUM_OF_MESSAGE / 2) ))
