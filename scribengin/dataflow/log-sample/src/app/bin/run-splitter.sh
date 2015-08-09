d10941c07ec1cf74c52e00569f19d16fb268543f
Tuan Nguyen <tuan08@gmail.com>
2015-08-09 20:35:31 +0700

    fix applications path

--- scribengin/dataflow/log-sample/src/app/bin/run-splitter.sh
+++ scribengin/dataflow/log-sample/src/app/bin/run-splitter.sh
@@ -71,32 +71,32 @@ fi
 SHELL=./scribengin/bin/shell.sh
 
 
-$SHELL vm upload-app --local $APP_DIR --dfs /apps/log-sample
+$SHELL vm upload-app --local $APP_DIR --dfs /applications/log-sample
 
 $SHELL vm submit \
-   --dfs-app-home /apps/log-sample \
+   --dfs-app-home /applications/log-sample \
    --registry-connect zookeeper-1:2181 --registry-db-domain /NeverwinterDP --registry-implementation com.neverwinterdp.registry.zk.RegistryImpl \
    --name vm-log-generator-1  --role vm-log-generator --vm-application  com.neverwinterdp.dataflow.logsample.vm.VMToKafkaLogMessageGeneratorApp \
-   --prop:report-path=/apps/log-sample/reports --prop:num-of-message=5000 --prop:message-size=512
+   --prop:report-path=/applications/log-sample/reports --prop:num-of-message=5000 --prop:message-size=512
 
 
 $SHELL dataflow submit \
-  --dfs-app-home /apps/log-sample \
+  --dfs-app-home /applications/log-sample \
   --dataflow-config $APP_DIR/conf/splitter/kafka-log-splitter-dataflow.json \
   --dataflow-id log-splitter-dataflow-1 --max-run-time 180000
 
 $SHELL vm wait-for-vm-status --vm-id vm-log-generator-1 --vm-status TERMINATED --max-wait-time 45000
-$SHELL registry dump --path /apps/log-sample
+$SHELL registry dump --path /applications/log-sample
 
 $SHELL dataflow wait-for-status --dataflow-id log-splitter-dataflow-1 --status TERMINATED
 $SHELL dataflow info --dataflow-id log-splitter-dataflow-1 --show-all
 
 
 $SHELL vm submit  \
-  --dfs-app-home /apps/log-sample \
+  --dfs-app-home /applications/log-sample \
   --registry-connect 127.0.0.1:2181  --registry-db-domain /NeverwinterDP --registry-implementation com.neverwinterdp.registry.zk.RegistryImpl \
   --name vm-log-validator-1 --role log-validator  --vm-application com.neverwinterdp.dataflow.logsample.vm.VMLogMessageValidatorApp \
-  --prop:report-path=/apps/log-sample/reports \
+  --prop:report-path=/applications/log-sample/reports \
   --prop:num-of-message-per-partition=5000 \
   --prop:wait-for-termination=300000 \
   --prop:validate-kafka=log4j.info,log4j.warn,log4j.error
@@ -104,4 +104,4 @@ $SHELL vm submit  \
 $SHELL vm wait-for-vm-status --vm-id vm-log-validator-1 --vm-status TERMINATED --max-wait-time 60000
 
 $SHELL vm info
-$SHELL registry dump --path /apps/log-sample
+$SHELL registry dump --path /applications/log-sample
