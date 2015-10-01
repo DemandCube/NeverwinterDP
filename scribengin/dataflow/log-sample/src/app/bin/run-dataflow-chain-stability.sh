a873641189a8f27d09102b17429905ef2c383eec
KingOfPoptart <richard.c.duarte@gmail.com>
2015-10-01 10:38:34 +0100

    Fixing elasticsearch default sink to be kafka

--- scribengin/dataflow/log-sample/src/app/bin/run-dataflow-chain-stability.sh
+++ scribengin/dataflow/log-sample/src/app/bin/run-dataflow-chain-stability.sh
@@ -4,5 +4,8 @@
 
 ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-$ROOT/run-dataflow-chain.sh --num-of-message=100000000  --generator-max-wait-time=30000 
+
+$ROOT/run-dataflow-chain.sh --num-of-message=25000000  --generator-max-wait-time=30000   --validator-disable --generator-send-period=1  --max-run-time=604800000
+#$ROOT/run-dataflow-chain.sh --num-of-message=25000000  --generator-max-wait-time=30000   --validator-disable --generator-send-period=5
+#$ROOT/run-dataflow-chain.sh --num-of-message=25000000  --generator-max-wait-time=60000 --generator-send-period=50 --max-run-time=86400000
 #--max-run-time=86400000 --validator-disable --dump-period=120000
