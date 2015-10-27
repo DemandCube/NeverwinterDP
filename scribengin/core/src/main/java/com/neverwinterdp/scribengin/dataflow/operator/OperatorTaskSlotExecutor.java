c04e1c67d4b25abbd7fc2e7d2c2ba3c47a47f352
Tuan Nguyen <tuan08@gmail.com>
2015-10-28 02:56:37 +0700

    Implement new dedicated task service

--- scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/operator/OperatorTaskSlotExecutor.java
+++ scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/operator/OperatorTaskSlotExecutor.java
@@ -49,7 +49,7 @@ public class OperatorTaskSlotExecutor extends TaskSlotExecutor<OperatorTaskConfi
     int recCount = 0;
     try {
       while(!interrupt && recCount <= 100 && !context.isComplete()) {
-        Record record = context.nextRecord(1);
+        Record record = context.nextRecord(10);
         if(record == null) break ;
 
         recCount++;
