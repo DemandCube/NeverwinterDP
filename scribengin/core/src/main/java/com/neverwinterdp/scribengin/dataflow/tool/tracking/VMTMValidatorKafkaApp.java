3f58d97370a088aacd9362492f4933e34f62c5db
Tuan Nguyen <tuan08@gmail.com>
2015-10-28 16:33:40 +0700

    change  validator algorithm

--- scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/tool/tracking/VMTMValidatorKafkaApp.java
+++ scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/tool/tracking/VMTMValidatorKafkaApp.java
@@ -113,10 +113,11 @@ public class VMTMValidatorKafkaApp extends VMApp {
                 KafkaPartitionReader pReader = readerQueue.take();
                 Record rec = null;
                 int count = 0;
-                while(count < 5000 && (rec = pReader.nextAs(Record.class, 1000)) != null) {
+                while((rec = pReader.nextAs(Record.class, 1000)) != null) {
                   TrackingMessage tMesg = JSONSerializer.INSTANCE.fromBytes(rec.getData(), TrackingMessage.class);
                   onTrackingMessage(tMesg);
                   count++;
+                  if(count == 5000) break;
                 }
                 readerQueue.offer(pReader);
               }
