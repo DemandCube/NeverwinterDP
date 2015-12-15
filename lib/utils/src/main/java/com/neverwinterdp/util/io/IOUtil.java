b277f2545419799b256db7eab9cbd6af848a56da
Tuan Nguyen <tuan08@gmail.com>
2015-12-15 15:48:41 +0700

    add compress/decompress util method

--- lib/utils/src/main/java/com/neverwinterdp/util/io/IOUtil.java
+++ lib/utils/src/main/java/com/neverwinterdp/util/io/IOUtil.java
@@ -13,6 +13,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Reader;
 import java.net.URL;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 /**
  * $Author: Tuan Nguyen$
@@ -162,4 +165,33 @@ public class IOUtil {
     in.close();
     return obj;
   }
+  
+  static public byte[] compress(byte[] data) throws IOException {  
+    Deflater deflater = new Deflater();  
+    deflater.setInput(data);  
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);   
+    deflater.finish();  
+    byte[] buffer = new byte[1024];   
+    while (!deflater.finished()) {  
+     int count = deflater.deflate(buffer); // returns the generated code... index  
+     outputStream.write(buffer, 0, count);   
+    }  
+    outputStream.close();  
+    byte[] output = outputStream.toByteArray();  
+    return output;  
+   }  
+
+  static public byte[] decompress(byte[] data) throws IOException, DataFormatException {  
+    Inflater inflater = new Inflater();   
+    inflater.setInput(data);  
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);  
+    byte[] buffer = new byte[1024];  
+    while (!inflater.finished()) {  
+     int count = inflater.inflate(buffer);  
+     outputStream.write(buffer, 0, count);  
+    }  
+    outputStream.close();  
+    byte[] output = outputStream.toByteArray();  
+    return output;  
+   }  
 }
