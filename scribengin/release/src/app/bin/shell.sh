bfb60da7fe56e182a38dc3da456c93207c2d3a97
Tuan Nguyen <tuan08@gmail.com>
2016-01-27 21:21:46 -0500

    add parameter to shell

--- scribengin/release/src/app/bin/shell.sh
+++ scribengin/release/src/app/bin/shell.sh
@@ -19,6 +19,7 @@ if [ "x$JAVA_HOME" == "x" ] ; then
   echo "WARNING JAVA_HOME is not set"
 fi
 
+
 (which $JAVACMD)
 isjava=$?
 
@@ -37,9 +38,22 @@ if $cygwin; then
   APP_DIR=`cygpath --absolute --windows "$APP_DIR"`
 fi
 
+if [ "x$SCRIBENGIN_REGISTRY_CONNECT" == "x" ] ; then 
+  SCRIBENGIN_REGISTRY_CONNECT="zookeeper-1:2181"
+fi
+SCRIBENGIN_HADOOP_MASTER="hadoop-master"
+
 JAVA_OPTS="-Xshare:auto -Xms128m -Xmx1024m -XX:-UseSplitVerifier" 
 APP_OPT="-Dapp.dir=$APP_DIR -Duser.dir=$APP_DIR"
-APP_OPT="$APP_OPT -Dshell.zk-connect=zookeeper-1:2181 -Dshell.hadoop-master=hadoop-master"
+APP_OPT="$APP_OPT -Dshell.zk-connect=$SCRIBENGIN_REGISTRY_CONNECT -Dshell.hadoop-master=$SCRIBENGIN_HADOOP_MASTER"
+
+echo "***********************************************************************************************************"
+echo "Run the shell command with the settings:"
+echo "  SCRIBENGIN_REGISTRY_CONNECT = $SCRIBENGIN_REGISTRY_CONNECT"
+echo "  SCRIBENGIN_HADOOP_MASTER    = $SCRIBENGIN_HADOOP_MASTER"
+echo "  JAVA_OPTS                   = $JAVA_OPTS"
+echo "  APP_OPT                     = $APP_OPT"
+echo "***********************************************************************************************************"
 
 MAIN_CLASS="com.neverwinterdp.scribengin.ShellMain"
 $JAVACMD -Djava.ext.dirs=$APP_DIR/libs:$JAVA_HOME/jre/lib/ext $JAVA_OPTS $APP_OPT $LOG_OPT $MAIN_CLASS "$@"
