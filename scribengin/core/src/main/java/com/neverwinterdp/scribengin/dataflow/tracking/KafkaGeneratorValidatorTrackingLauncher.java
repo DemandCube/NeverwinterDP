e8a97eadcc1160d3e900141569c0b73c102509f6
Tuan Nguyen <tuan08@gmail.com>
2016-01-31 11:22:42 +0700

    add simple dataflow launch for vm master test

--- scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/tracking/KafkaGeneratorValidatorTrackingLauncher.java
+++ scribengin/core/src/main/java/com/neverwinterdp/scribengin/dataflow/tracking/KafkaGeneratorValidatorTrackingLauncher.java
@@ -1,10 +1,13 @@
 package com.neverwinterdp.scribengin.dataflow.tracking;
 
+import com.beust.jcommander.Parameter;
 import com.neverwinterdp.registry.Registry;
 import com.neverwinterdp.scribengin.shell.ScribenginShell;
 import com.neverwinterdp.vm.client.VMClient;
 
 public class KafkaGeneratorValidatorTrackingLauncher extends TestTrackingLauncher {
+  @Parameter(names = "--validator-launch-delay", description = "")
+  private long validatorLaunchDelay = 300000;
   
   @Override
   public void execute(ScribenginShell shell, TrackingDataflowBuilder dflBuilder) throws Exception {
@@ -13,6 +16,9 @@ public class KafkaGeneratorValidatorTrackingLauncher extends TestTrackingLaunche
     
     GeneratorThread generatorThread = new GeneratorThread(registry, dflBuilder.getTrackingConfig());
     generatorThread.start();
+    
+    shell.console().println("The validator will be launched in " + validatorLaunchDelay + "ms, you need to manually launch the dataflow now");
+    Thread.sleep(validatorLaunchDelay);
     
     ValidatorThread validatorThread = new ValidatorThread(shell, dflBuilder);
     validatorThread.start();
