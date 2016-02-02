adbdaa2bc3dd75e726fde9c98d28f54a227bada1
Tuan Nguyen <tuan08@gmail.com>
2016-02-02 13:58:25 +0700

    fix simple dataflow test script

--- scribengin/dataflow/example/src/app/bin/run-simple.sh
+++ scribengin/dataflow/example/src/app/bin/run-simple.sh
@@ -7,6 +7,7 @@ fi
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 APP_DIR=`cd $bin/..; pwd; cd $bin`
+SCRIBENGIN_APP_DIR="../../../scribengin"
 
 export HADOOP_USER_NAME="neverwinterdp"
 
@@ -19,4 +20,4 @@ MAIN_CLASS="com.neverwinterdp.scribengin.dataflow.example.simple.SimpleDataflowE
 
 echo "Command: $JAVACMD -Djava.ext.dirs=$APP_DIR/libs:$JAVA_HOME/jre/lib/ext $JAVA_OPTS $MAIN_CLASS $@"
 
-$JAVACMD -Djava.ext.dirs=$APP_DIR/libs:$JAVA_HOME/jre/lib/ext $JAVA_OPTS $MAIN_CLASS "$@"
+$JAVACMD -Djava.ext.dirs=$SCRIBENGIN_APP_DIR/libs:$APP_DIR/libs:$JAVA_HOME/jre/lib/ext $JAVA_OPTS $MAIN_CLASS "$@"
