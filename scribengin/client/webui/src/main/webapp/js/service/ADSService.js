497679e9ed2203c6cb42549be9f33dcc89aa8153
Tuan Nguyen <tuan08@gmail.com>
2016-03-30 13:16:04 +0700

    fix cookie

--- scribengin/client/webui/src/main/webapp/js/service/ADSService.js
+++ scribengin/client/webui/src/main/webapp/js/service/ADSService.js
@@ -1,66 +1,9 @@
-function ADSVisitCookie() {
-  var guid = function() {
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-  };
-
-  var  deleteAllCookies = function() {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i];
-      var eqPos = cookie.indexOf("=");
-      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-  }
-
-  
-  var parseCookie = function() {
-    var cookies = document.cookie ;
-    var cookie = cookies.split(';');
-    var info = {} ;
-    for(var i = 0; i < cookie.length; i++) {
-      var nameValue = cookie[i].trim();
-      var pair  = nameValue.split('=');
-      var name  = pair[0];
-      var value = pair[1];
-      if('adsVisitorId' == name) info.adsVisitorId = value;
-      console.log("=> " + nameValue);
-    }
-    if(!info.adsVisitorId) info.adsVisitorId = guid();
-    return info;
-  };
-
-  this.info = parseCookie();
-
-  this.update = function() {
-    document.cookie = '';
-    var expireDate = new Date();
-    expireDate.setTime(expireDate.getTime() + (1* 24 * 60 * 60 * 1000));
-    var expires = "expires=" + expireDate.toUTCString();
-    var string = '';
-    for (var key in this.userInfo) {
-      if(this.userInfo.hasOwnProperty(key)) {
-        string += key + '=' + this.userInfo[key] + ";";
-        document.cookie = key + '=' + this.userInfo[key] + "; expires="+ expires + "; path=/";
-        console.log("add " + document.cookie) ;
-      }
-    }
-    //document.cookie = string + "expires="+ expires + "; path=/";
-  };
-
-  deleteAllCookies();
-  this.update();
-
-  this.getInfo = function() { return this.info; };
-}
-
 function ADSService(serviceUrl) {
-  var visitCookie = new ADSVisitCookie();
-
-  this.visitorId = visitCookie.getInfo().adsVisitorId
+  this.cookieManager = new CookieManager();
+  this.visitorId = this.cookieManager.getCookie('ads.visitorId');
+  if(!this.visitorId) {
+    this.visitorId = this.cookieManager.randomGUID();
+  }
 
   this.onClickADS = function(name, adUrl) {
     var adsEvent = {
--- scribengin/client/webui/src/main/webapp/js/service/InfoCollectorService.js
+++ scribengin/client/webui/src/main/webapp/js/service/InfoCollectorService.js
@@ -1,67 +1,17 @@
 function VisitCookie() {
-  var guid = function() {
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-  };
-
-
+  this.cookieManager = new CookieManager() ;
+  this.info = { };
   
-  var parseCookie = function() {
-    var cookies = document.cookie ;
-    var cookie = cookies.split(';');
-    var info = {} ;
-    for(var i = 0; i < cookie.length; i++) {
-      var nameValue = cookie[i].trim();
-      var pair  = nameValue.split('=');
-      var name  = pair[0];
-      var value = pair[1];
-      if('userId' == name) info.userId = value;
-      else if('visitorId' == name) info.visitorId = value;
-      //console.log("=> " + nameValue);
-    }
-    if(!info.visitorId) info.visitorId = guid();
-    return info;
-  };
-
-  this.userInfo = parseCookie();
-
-  this.update = function() {
-    document.cookie = '';
+  this.info.visitorId = this.cookieManager.getCookie('visitor.visitorId');
+  if(!this.info.visitorId) {
+    this.info.visitorId = this.cookieManager.randomGUID();
     var expireDate = new Date();
     expireDate.setTime(expireDate.getTime() + (1* 24 * 60 * 60 * 1000));
-    var expires = "expires=" + expireDate.toUTCString();
-    var string = '';
-    for (var key in this.userInfo) {
-      if(this.userInfo.hasOwnProperty(key)) {
-        string += key + '=' + this.userInfo[key] + ";";
-        document.cookie = key + '=' + this.userInfo[key] + "; expires="+ expires + "; path=/";
-        //console.log("add " + document.cookie) ;
-      }
-    }
-  };
-
-  this.deleteAllCookies = function() {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i];
-      var eqPos = cookie.indexOf("=");
-      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      if(this.userInfo[name]) {
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-      }
-    }
+    this.cookieManager.setCookie('visitor.visitorId', this.info.visitorId, "/", expireDate);
   }
 
-  //this.deleteAllCookies();
-  this.update();
-
-  this.getUserInfo = function() { 
-    return this.userInfo; 
-  };
+  this.getUserInfo = function() { return this.info; };
 }
-
 
 function GeoLocation() {
   var options = { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 };
@@ -111,8 +61,8 @@ function InfoCollectorService(serviceUrl) {
     screen: { width: screen.width, height:  screen.height },
 
     window: {
-     width:   window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
-     height:  window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+      width:   window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+      height:  window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
     },
 
     geoLocation: { 
